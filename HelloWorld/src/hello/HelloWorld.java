40c448d0523ca4463834dc57bf236cd8999adb3b
krzakj <miesek@github.com>
2015-03-01 17:46:11 +0100

    Small changes with helloworld

--- HelloWorld/src/hello/HelloWorld.java
+++ HelloWorld/src/hello/HelloWorld.java
@@ -4,7 +4,9 @@ package hello;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
         System.out.printf("Okienko");
+        System.out.printf("Dajemy dwa co to");
     }
 
 }
